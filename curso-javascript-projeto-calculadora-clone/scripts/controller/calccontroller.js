de352fc32d26956fc4e96647657aa116074ad746
Ronald Martins <ronny_nascimento@hotmail.com>
2021-12-08 23:19:44 -0300

    Inclusão do Construtor na classe

---
+++ curso-javascript-projeto-calculadora-clone/scripts/controller/calccontroller.js
@@ -0,0 +1,20 @@
+class CalcController {
+  constructor(){
+    
+    this._displayCalc;
+    this._dataAtual; 
+
+  }
+  get displayCalc (){
+    return this._displayCalc;
+  }
+  set displayCalc (valor){
+    this._displayCalc = valor;
+  }
+  get dataAtual(){
+    return this._dataAtual;
+  }
+  set dataAtual(valor){
+    this._dataAtual = valor;
+  }
+}
