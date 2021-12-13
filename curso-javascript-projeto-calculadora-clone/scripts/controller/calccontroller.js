6ddcac66f14e83ac551f392e88fd71e8c5750fa3
Ronald Martins <ronny_nascimento@hotmail.com>
2021-12-12 21:05:01 -0300

    Conclusão da Aula C08_Criação do Metodo AddEventListenerAll, para escutar de diversos eventos na calculadora.

--- curso-javascript-projeto-calculadora-clone/scripts/controller/calccontroller.js
+++ curso-javascript-projeto-calculadora-clone/scripts/controller/calccontroller.js
@@ -27,13 +27,27 @@ class CalcController {
     this.displayDate = this.currentDate.toLocaleTimeString(this._locale)
   }
 
+  addEventListenerAll(element, events, fn){
+
+    events.split(' ').forEach(event => {
+
+      element.addEventListener(event, fn, false);
+
+    });
+
+  }
+
   initButtonsEvents () {
+    
     let buttons = document.querySelectorAll ("#buttons > g, #parts > g");
 
     buttons.forEach((btn, index)=>{
-      buttons.addEventListener ('click', e=> {
+      this.addEventListenerAll(btn, "click drag mouseover", e=> {
         console.log(btn.className.baseVal.replace("btn-",""));
       })
+    });
+    this.addEventListenerAll (btn, "mouseover mouseup mousedown", e => {
+      btn.style.cursor = "pointer";
     })
     
   }
